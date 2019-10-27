0d74e075b5c04087cd38144a1ffc5d10acd8858e
Adi <andyguitarist18@gmail.com>
2019-10-27 16:20:56 +0100

    V2

--- app/src/main/java/pl/edu/pw/meil/knr/MainActivity.java
+++ app/src/main/java/pl/edu/pw/meil/knr/MainActivity.java
@@ -1,14 +1,17 @@
 package pl.edu.pw.meil.knr;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ImageButton connectBtn;
+    private ImageButton connectBtn;
+    private TextView connectionState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +19,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         connectBtn =  findViewById(R.id.connectBtn);
+        connectionState = findViewById(R.id.connectionStateTextView);
+
+        connectionState.setText("The Hal Rover is currently DISCONNECTED");
+        connectionState.setTextColor(getResources().getColor(R.color.colorPrimary));
+
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showToast(v);
+                connectionState.setText("The Hal Rover is currently CONNECTED");
+                connectionState.setTextColor(Color.parseColor("#00ff0"));
             }
         });
-    }
-
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG);
-        toast.show();
     }
 }
